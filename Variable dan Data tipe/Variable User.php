767f02f35b5f0e85d74a8f8620859c09e504f707
Nashwa putri sabina <92286033+Nashwaputri21@users.noreply.github.com>
2022-03-06 14:47:22 +0700

    Add files via upload

---
+++ Variable dan Data tipe/Variable User.php
@@ -0,0 +1,13 @@
+<?php
+// definisikan variables
+$nama = 'Nashwa Putri sabina';
+$umur = 19;
+$berat = 60;
+
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apakabar";
+
+?>
